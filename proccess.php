ed069053bff3ce02c27e96b3a860367a0cfacab7
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2022-08-05 20:03:49 +0200

    Control-Flow

--- proccess.php
+++ proccess.php
@@ -2,7 +2,16 @@
 
 // var_dump($_POST);
 
-echo $_POST['name'].'<br>'. $_POST['email'] . '<br>' . $_POST['password'] . '<br>' . $_POST['gender'];
+// echo $_POST['name'].'<br>'. $_POST['email'] . '<br>' . $_POST['password'] . '<br>' . $_POST['gender'];
+
+if (isset ($_POST['email']) && !empty($_POST['email']))
+{
+    echo $_POST['email'];
+}
+else
+{
+    echo "Please Enter Your Email";
+}
 
 
 ?>
