79b7d004420cc9eb4e6772854b4443e109875fc8
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2022-08-13 19:48:52 +0200

    php-connect

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php 
+
+// Open the connection 
+$conn = mysqli_connect("localhost","root","iti","blog");
+if (! $conn)
+{
+    echo mysqli_connect_error();
+    exit;
+}
+
+// Do the operation (SELECT, INSERT , ........)
+$query = "SELECT * FROM `users`";
+$result = mysqli_query($conn,$query);
+while ($row = mysqli_fetch_assoc($result))
+{
+    echo "Id : ".$row['id']."<br />";
+    echo "Name : ".$row['name']."<br />";
+    echo "Email : ".$row['email']."<br />";
+
+    echo str_repeat("-",50)."<br />";
+}
+
+// Close the connection 
+mysqli_free_result($result);
+mysqli_close($conn);
--- proccess.php
+++ proccess.php
@@ -4,13 +4,52 @@
 
 // echo $_POST['name'].'<br>'. $_POST['email'] . '<br>' . $_POST['password'] . '<br>' . $_POST['gender'];
 
+
+// name 
+if (isset ($_POST['name']) && !empty($_POST['name']))
+{
+    echo $_POST['name'];
+}
+else
+{
+    echo "Please Enter Your Name";
+}
+
+echo '<br>';
+
+// E-mail
 if (isset ($_POST['email']) && !empty($_POST['email']))
 {
     echo $_POST['email'];
 }
 else
 {
-    echo "Please Enter Your Email";
+    echo "Please Enter Your E-mail";
+}
+
+echo '<br>';
+
+// Password 
+
+if (isset ($_POST['password']) && !empty($_POST['password']))
+{
+    echo $_POST['password'];
+}
+else
+{
+    echo "Please Enter Your password";
+}
+
+echo '<br>';
+
+//Gender 
+if (isset ($_POST['gender']) && !empty($_POST['gender']))
+{
+    echo $_POST['gender'];
+}
+else
+{
+    echo "Please Enter Your Gender";
 }
 
 
